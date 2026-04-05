405bd7bfbd656ccdf31ba67957330b0e0b5091c6
dangthaison12 <thaison02004@gmail.com>
2026-04-05 23:19:58 +0700

    fix: align tests with actual implementations (EventService, ClubPost, Club controller signatures)

--- UNIC.ControllerTest/Controllers/ClubControllerTest.cs
+++ UNIC.ControllerTest/Controllers/ClubControllerTest.cs
@@ -44,7 +44,7 @@ namespace UNIC.ControllerTest.Controllers
             _mockClubService.Setup(s => s.GetAllAsync())
                             .ReturnsAsync(clubs);
 
-            var result = await _controller.GetAll();
+            var result = await _controller.GetAll(10, null, "1");
 
             var okResult = Assert.IsType<OkObjectResult>(result);
             Assert.Equal(200, okResult.StatusCode);
@@ -56,7 +56,7 @@ namespace UNIC.ControllerTest.Controllers
             _mockClubService.Setup(s => s.GetAllAsync())
                             .ReturnsAsync(new List<ClubResponseDto>());
 
-            var result = await _controller.GetAll();
+            var result = await _controller.GetAll(10, null, "1");
 
             Assert.IsType<OkObjectResult>(result);
         }
@@ -76,7 +76,7 @@ namespace UNIC.ControllerTest.Controllers
             _mockClubService.Setup(s => s.GetActiveClubsAsync())
                             .ReturnsAsync(clubs);
 
-            var result = await _controller.GetActiveClubs();
+            var result = await _controller.GetActiveClubs(10, null, "1");
 
             var okResult = Assert.IsType<OkObjectResult>(result);
             Assert.Equal(200, okResult.StatusCode);
